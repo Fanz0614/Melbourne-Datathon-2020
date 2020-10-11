0ba31890af6c1b1430fe0160f8bc68ff1003a27d
Fanz0614 <Wilber_fanz@outlook.com>
2020-10-11 17:29:33 +1100

    initial

---
+++ src/components/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../app.css';
+import HeroSection from '../HeroSection/index.js';
+
+function Home(){
+    return(
+        <div>
+            <HeroSection />
+        </div>
+    )
+}
+export default Home;
